73ab4b8f962dc9b5b4c475f1e30b6549ab2b6368
Amr Wael <93450935+amrrwael@users.noreply.github.com>
2025-01-20 15:47:22 +0700

    Delete Models/DTO's/UpdateUserProfileDto.cs

--- Models/DTO's/UpdateUserProfileDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PlatformSport.Models.Dto
-{
-    public class UserProfileUpdateDto
-    {
-        public string Name { get; set; }
-        public string PhoneNumber { get; set; }
-    }
-}
