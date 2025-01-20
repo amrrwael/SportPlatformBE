499480ed144189b59748093268faaa9027e6b047
Amr Wael <93450935+amrrwael@users.noreply.github.com>
2025-01-20 15:47:11 +0700

    Delete Models/DTO's/SportDto.cs

--- Models/DTO's/SportDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PlatformSport.Models.Dto
-{
-    public class SportDto
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }  // Name of the sport
-    }
-}
