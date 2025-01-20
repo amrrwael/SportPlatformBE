1104b6e25febfeb25b57e9d7a3df0632af3305ac
Amr Wael <93450935+amrrwael@users.noreply.github.com>
2025-01-20 15:54:35 +0700

    Add files via upload

---
+++ Models/DTO's/RoomDto.cs
@@ -0,0 +1,16 @@
+﻿// RoomDto.cs
+namespace PlatformSport.Models.Dto
+{
+    public class RoomDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int MaxPlayers { get; set; }
+        public DateTime EventStart { get; set; }
+        public int SportId { get; set; }
+        public int StadiumId { get; set; }  // Add this line
+        public string HostUserId { get; set; }
+        public List<string> PlayerIds { get; set; }  // List of player IDs in the room
+    }
+}
---
+++ Models/DTO's/SportDto.cs
@@ -0,0 +1,8 @@
+﻿namespace PlatformSport.Models.Dto
+{
+    public class SportDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }  // Name of the sport
+    }
+}
