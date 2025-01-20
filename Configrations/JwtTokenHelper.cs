686e362cd3cdd95b876ed206c4e92d751628c042
Amr Wael <93450935+amrrwael@users.noreply.github.com>
2025-01-20 15:57:32 +0700

    Update JwtTokenHelper.cs

--- Configrations/JwtTokenHelper.cs
+++ Configrations/JwtTokenHelper.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using PlatformSport.Configrations;
 using System.IdentityModel.Tokens.Jwt;
