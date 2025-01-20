f5bb767b5df06d6564ec35e6f268f423bf4ef231
Amr Wael <93450935+amrrwael@users.noreply.github.com>
2025-01-20 15:46:17 +0700

    Add files via upload

---
+++ Service/ISerivce/IRoomService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using PlatformSport.Database;
+using PlatformSport.Models;
+using PlatformSport.Models.Dto;
+
+namespace PlatformSport.Services
+{
+    public interface IRoomService
+    {
+        Task<RoomDto> CreateRoomAsync(CreateRoomDto dto, string hostUserId);
+        Task<RoomDto> GetRoomByIdAsync(int roomId);
+        Task<bool> JoinRoomAsync(int roomId, string userId);
+        Task<bool> LeaveRoomAsync(int roomId, string userId);
+    }
+}
