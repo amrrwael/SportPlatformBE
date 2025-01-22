777b5a263680aa4448a05618ee309ac182526c40
Amr Wael <93450935+amrrwael@users.noreply.github.com>
2025-01-22 19:40:02 +0700

    Update IStadiumService.cs

--- Service/ISerivce/IStadiumService.cs
+++ Service/ISerivce/IStadiumService.cs
@@ -1,13 +1,11 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
+// IStadiumService.cs
 using PlatformSport.Models.Dto;
 
-namespace PlatformSport.Services
+public interface IStadiumService
 {
-    public interface IStadiumService
-    {
-        Task<List<StadiumDto>> GetAllStadiumsAsync(int sportId);
-        Task<StadiumDto> GetStadiumByIdAsync(int stadiumId);
-        Task<int> CreateStadiumAsync(StadiumDto stadiumDto);
-    }
+    Task<List<StadiumDto>> GetAllStadiumsAsync(int sportId);
+    Task<StadiumDto> GetStadiumByIdAsync(int stadiumId);
+    Task<int> CreateStadiumAsync(StadiumDto stadiumDto);
+    Task<bool> UpdateStadiumAsync(int stadiumId, StadiumDto stadiumDto); // Add this method
+    Task<bool> DeleteStadiumAsync(int stadiumId); // Add this method
 }
