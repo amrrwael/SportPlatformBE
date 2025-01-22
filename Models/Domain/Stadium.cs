23a00c85243bbfbb30b1a5c90b59866525eb02e0
Amr Wael <93450935+amrrwael@users.noreply.github.com>
2025-01-22 19:36:25 +0700

    Update Stadium.cs

--- Models/Domain/Stadium.cs
+++ Models/Domain/Stadium.cs
@@ -1,10 +1,12 @@
-﻿namespace PlatformSport.Models
+
+namespace PlatformSport.Models
 {
     public class Stadium
     {
         public int Id { get; set; }
         public string Name { get; set; }  // Name of the stadium
         public string Location { get; set; }  // Location of the stadium
+        public CityEnum City { get; set; }  // City where the stadium is located (enum)
         public string Description { get; set; }  // A description of the stadium
         public decimal Price { get; set; }  // The price for renting the stadium (per hour or other metric)
 
