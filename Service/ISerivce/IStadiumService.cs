ad9063a364c36a7a5b1eaa3072ee9af4c7d94682
Amr Wael <93450935+amrrwael@users.noreply.github.com>
2025-01-24 03:03:41 +0700

    Update IStadiumService.cs

--- Service/ISerivce/IStadiumService.cs
+++ Service/ISerivce/IStadiumService.cs
@@ -4,8 +4,9 @@ using PlatformSport.Models.Dto;
 public interface IStadiumService
 {
     Task<List<StadiumDto>> GetAllStadiumsAsync(int sportId);
+    Task<List<StadiumDto>> GetAllStadiumsAsync(); // Add this method
     Task<StadiumDto> GetStadiumByIdAsync(int stadiumId);
     Task<int> CreateStadiumAsync(StadiumDto stadiumDto);
-    Task<bool> UpdateStadiumAsync(int stadiumId, StadiumDto stadiumDto); // Add this method
-    Task<bool> DeleteStadiumAsync(int stadiumId); // Add this method
+    Task<bool> UpdateStadiumAsync(int stadiumId, StadiumDto stadiumDto);
+    Task<bool> DeleteStadiumAsync(int stadiumId);
 }
